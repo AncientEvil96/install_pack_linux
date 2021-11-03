3231fd919144d5c3de55b1c8a8b5fc72b166b475
Ilya Efimov <bersek123@gmail.com>
2021-11-03 19:07:30 +0300

    123

--- install_pac.sh
+++ install_pac.sh
@@ -10,6 +10,7 @@
 # sudo apt-get install -y mc
 # sudo apt-get install -y ssh
 # sudo apt-get install -y git
+# sudo apt-get install -y network-manager-openvpn
 # sudo apt install -y openjdk-11-jre-headless
 
 ## sublime
@@ -27,6 +28,8 @@
 # sudo apt install -y docker-ce
 # sudo chmod 666 /var/run/docker.sock
 # sudo usermod -aG docker ${USER}
+# sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compose
+# sudo chmod +x /usr/local/bin/docker-compose
 
 # установка драйвера тачпаада
 # sudo apt update -y
@@ -46,7 +49,20 @@
 
 # что бы вход наушников работал только под рутом
 # sudo passwd root
-sudo su
-echo "options snd-hda-intel position fix=1" >> /etc/modprobe.d/alsa-base.conf
-echo "options snd-hda-intel model=,dell-headset-multi" >> /etc/modprobe.d/alsa-base.conf
-exit
+# sudo su
+# echo "options snd-hda-intel position fix=1" >> /etc/modprobe.d/alsa-base.conf
+# echo "options snd-hda-intel model=,dell-headset-multi" >> /etc/modprobe.d/alsa-base.conf
+# exit
+
+# sbt
+# sudo apt-get update
+# sudo apt-get install apt-transport-https curl gnupg -yqq
+# echo "deb https://repo.scala-sbt.org/scalasbt/debian all main" | sudo tee /etc/apt/sources.list.d/sbt.list
+# echo "deb https://repo.scala-sbt.org/scalasbt/debian /" | sudo tee /etc/apt/sources.list.d/sbt_old.list
+# curl -sL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x2EE0EA64E40A89B84B2DF73499E82A75642AC823" | sudo -H gpg --no-default-keyring --keyring gnupg-ring:/etc/apt/trusted.gpg.d/scalasbt-release.gpg --import
+# sudo chmod 644 /etc/apt/trusted.gpg.d/scalasbt-release.gpg
+# sudo apt-get update
+# sudo apt-get install sbt
+
+#net-tools
+# sudo apt install net-tools
